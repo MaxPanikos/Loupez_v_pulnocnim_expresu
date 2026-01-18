d099895a6a7154daf9b9b0b09e5711b091e14acc
OmegaSusik <maxpanik1@gmail.com>
2026-01-18 13:47:06 +0100

    added some methods

--- src/Characters/Character.java
+++ src/Characters/Character.java
@@ -1,4 +1,55 @@
 package Characters;
 
+import Rooms.Room;
+
 public abstract class Character {
+    protected String name;
+    protected int age;
+    protected Room currentRoom;
+
+    public Character(String name, int age, Room currentRoom) {
+        this.name = name;
+        this.age = age;
+        this.currentRoom = currentRoom;
+    }
+
+    public boolean move (Room newRoom) {
+        if (currentRoom.getName().equals(newRoom.getName())) {
+            return false;
+        }
+        if (currentRoom.getRooms().containsKey(newRoom.getName())) {
+            currentRoom.removeCharacter(this);
+            newRoom.addCharacter(this);
+            return true;
+        }
+        return false;
+    }
+
+    public String ask () {
+        return "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
 }
--- src/Rooms/Room.java
+++ src/Rooms/Room.java
@@ -1,4 +1,52 @@
 package Rooms;
 
+import Items.Item;
+import Characters.Character;
+
+import java.util.HashMap;
+
 public abstract class Room {
+    protected String name;
+    protected HashMap<String, Character> characters;
+    protected HashMap<String, Item> items;
+    protected HashMap<String, Room> rooms;
+
+    public Room(String name) {
+        this.name = name;
+        this.characters = new HashMap<>();
+        this.items = new HashMap<>();
+        this.rooms = new HashMap<>();
+    }
+
+    public boolean addCharacter(Character character) {
+        if (!characters.containsKey(character.getName()) && character.getCurrentRoom().getRooms().containsKey(name)) {
+            characters.put(character.getName(), character);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeCharacter(Character character) {
+        if (characters.containsKey(character.getName())) {
+            characters.remove(character.getName());
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HashMap<String, Character> getCharacters() {
+        return characters;
+    }
+
+    public HashMap<String, Item> getItems() {
+        return items;
+    }
+
+    public HashMap<String, Room> getRooms() {
+        return rooms;
+    }
 }
