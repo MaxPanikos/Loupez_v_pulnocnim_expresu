2b8b2cbd52bfe01c543449bf6e890f16b2b86394
panik <lolekossusig@gmail.com>
2026-02-23 09:51:34 +0100

    PlayerTest.java

---
+++ Test/PlayerTest.java
@@ -0,0 +1,26 @@
+class PlayerTest {
+
+    @org.junit.jupiter.api.Test
+    void checkInventory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getInventoryItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void grabItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void throwAwayItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+}
